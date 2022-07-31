cc7eca65a2b85f952d99fa35f81e64ecaf9ce23e
Nikola <woleet@outlook.com>
2022-07-31 19:24:19 +0200

    Rich comments and action text

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-User.create(email: 'woleet@example.com', password: 'password', password_confirmation: 'password')
+User.create(email: 'woleet@example.com', name: "Wolee T.", password: 'password', password_confirmation: 'password')
 
 10.times do |x|
   Post.create(title: "Title #{x} from seeded post", body: "Body #{x} from seeded posts", user_id: User.first.id)
